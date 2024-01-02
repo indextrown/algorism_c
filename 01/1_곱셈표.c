2169d222636c724ffbdad81e523ae336069401f0
insta2_profile <69367698+indextrown@users.noreply.github.com>
2024-01-02 18:33:11 +0900

    Update 1_곱셈표.c
    코드완성

--- 01/1_곱셈표.c
+++ 01/1_곱셈표.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("곱셈표\n");
+
+    printf("   |");
+    for (int i=1; i<=9; i++) printf("%3d", i);
+    printf("\n");
+    printf("---");
+    printf("+");
+    for (int i=1; i<=9; i++) printf("---");
+    printf("\n");
+    for (int i=1; i<=9; i++)
+    {
+        printf("%3d|", i);
+        for (int j=1; j<=9; j++)
+        {
+            printf("%3d", i*j);
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
 
